60246d3b1d6074a0d4793b2469681044d840ce47
mvantil@kc.rr.com <mvantil@kc.rr.com@eebaaf53-5d55-0410-82d7-0f62e5d59e31>
2009-04-03 03:33:19 +0000

    Missed a couple of files.

---
+++ trunk/UtilityCore/ExceptionExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Utility.Core
+{
+    public class ExceptionWrapper : Exception
+    {
+        private readonly Exception _innerException;
+
+        public ExceptionWrapper(Exception innerException)
+        {
+            _innerException = innerException;
+        }
+    }
+
+    public static class ExceptionExtensions
+    {
+        public static void Rethrow(this Exception src)
+        {
+            throw new ExceptionWrapper(src);
+        }
+    }
+}
