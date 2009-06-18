4e886e5bec7e99538f7d454fe8c6abec148f5749
mvantil@kc.rr.com <mvantil@kc.rr.com@eebaaf53-5d55-0410-82d7-0f62e5d59e31>
2009-06-18 20:58:37 +0000

    Add files added to extensions.

---
+++ trunk/UtilityCore/Extensions/ExceptionExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace Utility.Core
+{
+    public class ExceptionWrapper : Exception
+    {
+        private readonly Exception _innerException;
+
+        public ExceptionWrapper(Exception innerException)
+        {
+            _innerException = innerException;
+        }
+    }
+
+    public static class ExceptionExtensions
+    {
+        public static void Rethrow(this Exception src)
+        {
+            throw new ExceptionWrapper(src);
+        }
+
+        public static string FormatError(this Exception src)
+        {
+            var inner = src.ChainWithSelf(e => e.InnerException).Last();
+            return inner.Message + "\r\n\r\n\r\n" + inner.StackTrace;
+        }
+    }
+}
